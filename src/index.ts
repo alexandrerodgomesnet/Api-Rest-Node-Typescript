0b590cdea2308feedc118471c998b5171380796f
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-01 11:38:24 -0300

    Setup inicial.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { server } from './server/Server';
+
+
+server.listen(3335, () => console.log('Servidor rodando com sucesso!'));
---
+++ src/server/Server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.get('/', (req: Request, res: Response) => {
+    return res.send('Hello World!')
+});
+
+
+export { server };
