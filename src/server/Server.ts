87485948c44ea2e8b2d67812b046cd7286fe901e
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-01 12:32:39 -0300

    COnfigurando Server e Routes.

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes';
 
 const server = express();
 
-server.get('/', (req: Request, res: Response) => {
-    return res.send('Hello World!');
-});
+server.use(express.json());
 
+server.use(router);
 
 export { server };
